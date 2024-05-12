c4956da2328eebaa8e50ed497591038ac0fd3d8c
Juan Manuel <isazamanuel04@gmail.com>
2024-05-12 13:03:40 -0500

    c1

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let carrito = [];
-let ventasTotales = 0;
+let ventasTotales = [];
 
 const productosDisponibles = [
-    { id: 1, nombre: "Mandingas", Descripcion: "Salchipapas con salsa de la Casa", Precio: 7000, cantidad: 10 },
-    { id: 2, nombre: "CocaCola", Descripcion: "Gaseosa", Precio: 3000, cantidad: 15 },
-    { id: 3, nombre: "Burguer", Descripcion: "Hamburguesa especial con carne y tocineta", Precio: 16000, cantidad: 10 },
-    { id: 4, nombre: "Limonada", Descripcion: "Bebida Natural", Precio: 4000, cantidad: 10 },
-    { id: 5, nombre: "Arepa Especial", Descripcion: "Arepa rellena con todo", Precio: 10000, cantidad: 10 },
-]
+    { id: 1, nombre: "Mandingas", descripcion: "Salchipapas con salsa de la Casa", precio: 7000, cantidad: 10 },
+    { id: 2, nombre: "CocaCola", descripcion: "Gaseosa", precio: 3000, cantidad: 15 },
+    { id: 3, nombre: "Burguer", descripcion: "Hamburguesa especial con carne y tocineta", precio: 16000, cantidad: 10 },
+    { id: 4, nombre: "Limonada", descripcion: "Bebida Natural", precio: 4000, cantidad: 10 },
+    { id: 5, nombre: "Arepa Especial", descripcion: "Arepa rellena con todo", precio: 10000, cantidad: 10 },
+];
 
 function mostrarMenu() {
-    console.log("Bienvenido al carrito de compras: ")
+    console.log("Bienvenido al carrito de compras: ");
     console.log("1. Agregar producto al carrito");
     console.log("2. Ver carrito");
     console.log("3. Ver total de la compra");
@@ -19,19 +19,17 @@ function mostrarMenu() {
     console.log("6. Salir");
 }
 
-
 function agregarProducto() {
     console.log("Productos disponibles:");
     productosDisponibles.forEach((producto, index) => {
-        console.log(`${index + 1}). ${producto.nombre} - $${producto.Precio} - ${producto.Descripcion} - Cantidad disponible: ${producto.cantidad}`);
+        console.log(`${index + 1}). ${producto.nombre} - $${producto.precio} - ${producto.descripcion} - Cantidad disponible: ${producto.cantidad}`);
     });
 
-    let opcionProducto = parseInt(prompt("Seleccione el numero del producto que desea agregar:"));
+    let opcionProducto = parseInt(prompt("Seleccione el número del producto que desea agregar:"));
     if (opcionProducto < 1 || opcionProducto > productosDisponibles.length) {
         console.log("Cantidad inválida. Por favor, ingrese una cantidad válida.");
         return;
     }
-
 
     let cantidadProducto = parseInt(prompt("Ingrese la cantidad de productos que desea agregar:"));
     if (isNaN(cantidadProducto) || cantidadProducto <= 0) {
@@ -39,46 +37,39 @@ function agregarProducto() {
         return;
     }
 
+    let productoSeleccionado = productosDisponibles[opcionProducto - 1];
+    let totalVenta = productoSeleccionado.precio * cantidadProducto;
+    carrito.push({ productoSeleccionado, cantidad: cantidadProducto, total: totalVenta });
+    ventasTotales.push(totalVenta);
 
-    let productoSeleccionado = productosDisponibles[opcionProducto - 1]
-    let indexProductoEnCarrito = carrito.findIndex(productoEnCarrito => productoEnCarrito.id === productoSeleccionado.id)
-    if (indexProductoEnCarrito !== -1) {
-        carrito[indexProductoEnCarrito].cantidad += cantidadProducto;
-    } else {
-        carrito.push({ productoSeleccionado, cantidad: cantidadProducto })
-
-    }
-
-    console.log(`Se han agregado ${cantidadProducto} ${productoSeleccionado.nombre} al carrito de compras. `)
-    ventasTotales++;
+    console.log(`Se han agregado ${cantidadProducto} ${productoSeleccionado.nombre} al carrito de compras.`);
 }
 
-
 function verCarrito() {
-    console.log("Productos en el carrito")
+    console.log("Productos en el carrito:");
     if (carrito.length === 0) {
-        console.log("El carrito esta vacío")
+        console.log("El carrito está vacío.");
     } else {
-        carrito.forEach((producto, index) => {
-            console.log(`${index + 1}, ${producto.nombre} - Cantidad ${producto.cantidad}`);
-        })
+        carrito.forEach((item, index) => {
+            console.log(`${index + 1}, ${item.productoSeleccionado.nombre} - Cantidad: ${item.cantidad}`);
+        });
     }
 }
 
 function verTotalCompra() {
-    let totalCompra = carrito.reduce((total, producto) => total + (producto.precio * producto.cantidad), 0);
-    console.log(`Total de compras ${totalCompra}`)
+    let totalCompra = carrito.reduce((total, producto) => total + producto.total, 0);
+    console.log(`Total de compras: $${totalCompra}`);
 }
 
 function verCantidadTotalProductos() {
-    let cantidadTotal = carrito.reduce((total, producto) => total + producto.cantidad, 0)
-    console.log(`Cantidad total de productos en el carrito: ${cantidadTotal}`)
+    let cantidadTotal = carrito.reduce((total, producto) => total + producto.cantidad, 0);
+    console.log(`Cantidad total de productos en el carrito: ${cantidadTotal}`);
 }
 
-function verVentasTotales(){
-    console.log(`Ver ventas totales del dia ${ventasTotales}`)
+function verVentasTotales() {
+    let totalVentasDiarias = ventasTotales.reduce((total, venta) => total + venta, 0);
+    console.log(`Ventas totales del día: $${totalVentasDiarias}`);
 }
-
 
 function ejecutarCarrito() {
     let opcion;
@@ -87,19 +78,19 @@ function ejecutarCarrito() {
         opcion = parseInt(prompt("Seleccione una opción"));
         switch (opcion) {
             case 1:
-                agregarProducto()
+                agregarProducto();
                 break;
             case 2:
-                verCarrito()
+                verCarrito();
                 break;
             case 3:
-                verTotalCompra()
+                verTotalCompra();
                 break;
             case 4:
-                verCantidadTotalProductos()
+                verCantidadTotalProductos();
                 break;
             case 5:
-                verVentasTotales()
+                verVentasTotales();
                 break;
             case 6:
                 console.log("Gracias por usar el carrito de compras.");
@@ -110,4 +101,4 @@ function ejecutarCarrito() {
     } while (opcion !== 6);
 }
 
-ejecutarCarrito()
+ejecutarCarrito();
